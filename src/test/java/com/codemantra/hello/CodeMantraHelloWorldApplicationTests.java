9ca69c7e6ec51a9447bb849e659c3ef31540823e
Suvendu Chatterjee <suvendu.chatterjee97@gmail.com>
2025-07-05 13:23:35 +0530

    first commit

---
+++ src/test/java/com/codemantra/hello/CodeMantraHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemantra.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMantraHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
